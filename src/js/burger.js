4fe19b7b244d69b661b9be896731616929f2b234
Solya <catsubrosa@gmail.com>
2024-06-19 13:43:41 +0300

    Add header top part

--- src/js/burger.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", initBurger);
-
-function initBurger () {
-    const burger = document.querySelector('.header__burger');
-    const menu = document.querySelector('.header__buttons-container');
-    burger.addEventListener('click', () => {
-        menu.classList.toggle('show');
-    })
-}
